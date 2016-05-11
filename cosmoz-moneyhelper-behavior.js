8a3938b4dc03673577ceecc630ff8d91baa3c2ea
Patrik Kullman <patrik.kullman@neovici.se>
2016-05-11 21:17:12 +0200

    Add USD display setting

--- cosmoz-moneyhelper-behavior.js
+++ cosmoz-moneyhelper-behavior.js
@@ -49,6 +49,20 @@ if (typeof Cosmoz === 'undefined') {
 				thousand: ",",
 				decimal: "."
 			}
+		},
+		USD: {
+			currency: {
+				symbol : "$",   // default currency symbol is '$'
+				format: "%s %v", // controls output: %s = symbol, %v = value/number (can be object: see below)
+				decimal : ".",  // decimal point separator
+				thousand: ",",  // thousands separator
+				precision : 2   // decimal places
+			},
+			number: {
+				precision : 0,  // default precision on numbers is 0
+				thousand: ",",
+				decimal : "."
+			}
 		}
 	};
 
