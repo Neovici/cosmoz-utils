e22ab915db1fc6c6a0784a1166fa25dfdc9acc14
Meghea Iulian <iulian.meghea@gmail.com>
2020-05-25 10:45:30 +0300

    chore(test): adds use-debounce-raf tests

---
+++ test/use-debounce-raf.test.js
@@ -0,0 +1,40 @@
+import { useDebounceRaf } from '../lib/hooks/use-debounce-raf';
+import { component } from 'haunted';
+import {
+	assert,
+	html,
+	fixture,
+	nextFrame
+} from '@open-wc/testing';
+
+customElements.define(
+	'use-debounce-raf',
+	component(function ({ value }) {
+		// eslint-disable-next-line no-invalid-this
+		this.current = useDebounceRaf(value);
+	})
+);
+
+
+suite('use-debounce-raf', () => {
+	test('debounced', async () => {
+		const value = 1,
+			result = await fixture(html`<use-debounce-raf .value=${ value } />`);
+		assert.equal(result.current, value);
+
+		result.value = 2;
+		assert.equal(result.current, 1);
+
+		// 1st frame
+		await nextFrame();
+		assert.equal(result.current, 1);
+
+		//2nd frame
+		await nextFrame();
+		assert.equal(result.current, 1);
+
+		//3rd frame
+		await nextFrame();
+		assert.equal(result.current, 2);
+	});
+});
