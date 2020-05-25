df81d442ca91f3495ff28713bda7b52fc483e465
Meghea Iulian <iulian.meghea@gmail.com>
2020-05-25 12:19:36 +0300

    chore(test): adds use-drop-files, use-handle-drop tests

---
+++ test/use-dropped-files.test.js
@@ -0,0 +1,32 @@
+import { useDroppedFiles } from '../lib/hooks/use-dropped-files';
+import { component } from 'haunted';
+import {
+	assert,
+	html,
+	fixture,
+	nextFrame
+} from '@open-wc/testing';
+
+customElements.define(
+	'use-dropped-files',
+	component(function ({ el }) {
+		// eslint-disable-next-line no-invalid-this
+		this.current = useDroppedFiles(el);
+	})
+);
+
+
+suite('use-dropped-files', () => {
+	test('dropped files', async () => {
+		const el = document.createElement('div'),
+			result = await fixture(html`<use-dropped-files .el=${ el } />`),
+			file = new File(['foo'], 'foo.txt', {
+				type: 'text/plain'
+			});
+
+		el.dispatchEvent(Object.assign(new Event('drop'), { dataTransfer: { files: [file]}}));
+
+		await nextFrame();
+		assert.equal(result.current[0], file);
+	});
+});
