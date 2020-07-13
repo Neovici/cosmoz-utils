d81316b6c892e4cc831b5cfecb64610c4e1c2a0a
Cristian Necula <virusz@gmail.com>
2020-07-13 12:52:47 +0300

    fix(haunted-polymer): haunted state is updated even if only haunted props have changed
    This leads to infinite loops when the haunted props are not 100% memoized.

--- test/haunted-polymer.test.js
+++ test/haunted-polymer.test.js
@@ -8,6 +8,7 @@ import {
 
 import { hauntedPolymer } from '../lib/haunted-polymer.js';
 import { useMemo } from 'haunted';
+import { spy } from 'sinon';
 
 const useTestHook = element => {
 	return useMemo(() => {
@@ -91,8 +92,6 @@ suite('haunted polymer render lit', () => {
 	});
 });
 
-
-
 const useSum = ({
 	a, b
 }) => {
@@ -125,5 +124,69 @@ suite('haunted polymer without outputPath', () => {
 		calculator.a = 5;
 		await nextFrame();
 		assert.equal(calculator.shadowRoot.textContent, '5 + 2 = 7');
+	});
+});
+
+const useLitSum = ({
+	a, b
+}) => {
+	return {
+		sum: html`${ a + b }`,
+		diff: b - a
+	};
+};
+
+class myLitCalculator extends hauntedPolymer(useLitSum)(PolymerElement) {
+	static get properties() {
+		return {
+			a: {
+				type: Number
+			},
+			b: {
+				type: Number
+			},
+			// normally you should omit listing props that are set by the haunted hook
+			// if you *need* to define a haunted prop as a polymer prop (for notify purposes for example), mark them as haunted prop
+			diff: {
+				type: Number,
+				haunted: true,
+				notify: true
+			}
+		};
+	}
+
+	static get template() {
+		return polymerHtml`
+			[[ a ]] + [[ b ]] = <span id="sum"></span>
+			[[ b ]] - [[ a ]] = [[ diff ]]
+		`;
+	}
+
+	static get observers() {
+		return ['renderLitTo(sum, $.sum)'];
+	}
+}
+
+customElements.define('my-lit-calculator', myLitCalculator);
+
+suite('haunted polymer with lit rendering and notifying props', () => {
+	test('basic', async () => {
+		const diffChanged = spy(),
+			calculator = await fixture(html`<my-lit-calculator a="1" b="2" @diff-changed=${ diffChanged }></my-lit-calculator>`);
+
+		spy(calculator, 'renderLitTo');
+
+		await nextFrame();
+		assert.equal(calculator.shadowRoot.textContent.replace(/[\n\t]/gui, ''), '1 + 2 = 32 - 1 = 1');
+
+		calculator.a = 5;
+		await nextFrame();
+		await nextFrame();
+		assert.equal(calculator.shadowRoot.textContent.replace(/[\n\t]/gui, ''), '5 + 2 = 72 - 5 = -3');
+
+		await nextFrame();
+		await nextFrame();
+		assert.lengthOf(calculator.renderLitTo.getCalls(), 1);
+		assert.isTrue(diffChanged.calledTwice);
 	});
 });
