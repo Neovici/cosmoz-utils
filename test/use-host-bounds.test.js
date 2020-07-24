2d172c83381aaefc61a70f542e3108469a23eb6c
Meghea Iulian <iulian.meghea@gmail.com>
2020-07-24 12:53:11 +0300

    fix: adds use-host-bounds

---
+++ test/use-host-bounds.test.js
@@ -0,0 +1,27 @@
+import { useHostBounds } from '../lib/hooks/use-host-bounds.js';
+import { component } from 'haunted';
+import {
+	assert,
+	html,
+	fixtureSync,
+	nextFrame
+} from '@open-wc/testing';
+
+customElements.define(
+	'use-host-bounds',
+	component(function () {
+		// eslint-disable-next-line no-invalid-this
+		this.current = useHostBounds();
+	})
+);
+
+suite('use-host-bounds', () => {
+	test('sizing', async () => {
+		const result = await fixtureSync(html`<use-host-bounds style="width: 120px; display:block"></use-host-bounds>`);
+
+		await nextFrame();
+		await nextFrame();
+
+		assert.equal(Math.round(result.current.width), 120);
+	});
+});
