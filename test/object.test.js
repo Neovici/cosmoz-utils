76d15247e56dbb1d0c3ad2e42de890bff1889241
Meghea Iulian <iulian.meghea@gmail.com>
2021-03-17 09:41:25 +0200

    feat(object): add merge utility

--- test/object.test.js
+++ test/object.test.js
@@ -2,7 +2,8 @@ import { assert } from '@open-wc/testing';
 
 import {
 	prop,
-	strProp
+	strProp,
+	merge
 } from '../lib/object';
 import { identity } from '../lib/function';
 
@@ -16,6 +17,44 @@ suite('prop', () => {
 	test('strProp', () => {
 		assert.equal(strProp('b')({ b: 2 }), '2');
 		assert.equal(strProp('b')({ c: 4 }), '');
+	});
+});
+
+suite('merge', () => {
+	test('merges', () => {
+		assert.deepEqual(merge({ a: 1 }, { b: 2 }), {
+			a: 1,
+			b: 2
+		});
+		assert.deepEqual(merge({
+			a: 1,
+			b: 3
+		}, { b: 2 }), {
+			a: 1,
+			b: 2
+		});
+		assert.deepEqual(merge({
+			a: 1,
+			b: 3
+		}, { b: 2 }, null), {
+			a: 1,
+			b: 2
+		});
+	});
+
+	test('merges objects', () => {
+		assert.deepEqual(merge({ a: { a1: 1 }}, { a: { a2: 2 }}), {
+			a: {
+				a1: 1,
+				a2: 2
+			}
+		});
+	});
+
+	test('merges array', () => {
+		assert.deepEqual(merge({ a: [1, 2]}, { a: [3, 4]}), {
+			a: [1, 2, 3, 4]
+		});
 	});
 });
 
