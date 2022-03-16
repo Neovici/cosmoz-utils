4c307ade229cc44d548d56064491fdd58d5f645a
Cristian Necula <virusz@gmail.com>
2022-03-16 17:01:54 +0200

    feat(debounce$): generic async function debouncer

--- test/promise.test.js
+++ test/promise.test.js
@@ -1,6 +1,6 @@
-import { assert, nextFrame } from '@open-wc/testing';
+import { assert, aTimeout, nextFrame } from '@open-wc/testing';
 import { spy } from 'sinon';
-import { limit$ } from '../lib/promise';
+import { debounce$, limit$ } from '../lib/promise';
 
 suite('limit$', () => {
 	test('rate limits a function', async () => {
@@ -48,5 +48,52 @@ suite('limit$', () => {
 		} catch (e) {
 			assert.equal(e.message, 'broken');
 		}
+	});
+
+	test('also works with sync functions', async () => {
+		const double = a => a * 2,
+			double$ = limit$(double, 2);
+		assert.equal(await double$(2), 4);
+	});
+});
+
+suite('debounce$', () => {
+	test('debounces an async function', async () => {
+		const callLater = callback =>
+				new Promise(resolve =>
+					requestAnimationFrame(() => resolve(callback()))
+				),
+			callLater$ = debounce$(callLater, 50),
+			callback = spy();
+
+		callLater$(callback);
+
+		await nextFrame();
+		assert.isFalse(callback.called);
+
+		callLater$(callback);
+
+		await nextFrame();
+		assert.isFalse(callback.called);
+
+		await aTimeout(50);
+		assert.isTrue(callback.called);
+	});
+
+	test('all debounced calls resolve with the same data', async () => {
+		const fetch = x =>
+				new Promise(resolve => requestAnimationFrame(() => resolve(x * 2))),
+			fetch$ = debounce$(fetch, 50),
+			result1 = fetch$(1);
+
+		await nextFrame();
+
+		const result2 = fetch$(2);
+
+		await nextFrame();
+
+		const result3 = fetch$(3);
+
+		assert.deepEqual(await Promise.all([result1, result2, result3]), [6, 6, 6]);
 	});
 });
