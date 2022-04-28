e23e1a26d1869597c8d3863a462b7773c6dec26b
Luta Vasile <elsile69@yahoo.com>
2022-04-28 06:56:18 +0300

    feat(icons): add svg icons
    To replace polymer icons with svg icons.

---
+++ elements/cz-spinner.js
@@ -0,0 +1,26 @@
+import {
+	component, html
+} from 'haunted';
+
+const Spinner = () => html`<style>
+	@keyframes rotating {
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
+	:host {
+		display: inline-block;
+		vertical-align: middle;
+		border-radius: 50%;
+		width: 22px;
+		height: 22px;
+		border: 2px solid rgba(0, 0, 0, 0.1);
+		border-top: 2px solid #5f5a92;
+		animation: rotating 1.2s infinite cubic-bezier(0.785, 0.135, 0.15, 0.86);
+		box-sizing: border-box;
+		margin: 0 4px;
+	}
+</style>`;
+
+customElements.define('cz-spinner', component(Spinner));
