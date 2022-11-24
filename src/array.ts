520ac4681c478b6b661ffbd8b6f3af2c0e665518
Meghea Iulian <iulian.meghea@gmail.com>
2022-11-24 08:07:53 +0200

    fix(array): add better typing
    Improve the typing of `array` function.

--- src/array.ts
+++ src/array.ts
@@ -4,27 +4,31 @@ const isIterable = (x: unknown): x is Iterable<unknown> => {
 	return typeof x === 'object' && x !== null && Symbol.iterator in x;
 };
 
-export const array = (arr: unknown) => {
-		if (arr == null) {
-			return [];
-		}
-		if (Array.isArray(arr)) {
-			return arr;
-		}
-		if (typeof arr === 'string') {
-			return [arr];
-		}
-		if (isIterable(arr)) {
-			return Array.from(arr);
-		}
+export function array(): [];
+export function array(arr: null): [];
+export function array<T>(arr: T[]): T[];
+export function array<T>(arr: Iterable<T>): T[];
+export function array<T = unknown>(arr: T): T[];
+export function array(arr?: unknown) {
+	if (arr == null) {
+		return [];
+	}
+	if (Array.isArray(arr)) {
+		return arr;
+	}
+	if (typeof arr === 'string') {
 		return [arr];
-	},
-	// TODO: Improve definition
-	without =
-		(exclude: unknown, predicate = identity) =>
-		(list: unknown) => {
-			const excludes = array(exclude).map(predicate);
-			return array(list).filter(
-				(value) => !excludes.includes(predicate(value))
-			);
-		};
+	}
+	if (isIterable(arr)) {
+		return Array.from(arr);
+	}
+	return [arr];
+}
+
+// TODO: Improve definition
+export const without =
+	(exclude: unknown, predicate = identity) =>
+	(list: unknown) => {
+		const excludes = array(exclude).map(predicate);
+		return array(list).filter((value) => !excludes.includes(predicate(value)));
+	};
