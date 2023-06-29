0f7b47d8ba81a5f39d5f1b581b7bd4189e8c61c4
Cristian Necula <virusz@gmail.com>
2023-06-29 19:39:23 +0300

    feat(directives): provide and consume context values
    Helper directives for simplifying usage of contexts.
    Check out the tests for examples.

---
+++ src/directives/provide.ts
@@ -0,0 +1,61 @@
+import { Context, ContextDetail } from 'haunted/lib/create-context';
+import { contextEvent } from 'haunted/lib/symbols';
+import { noChange, nothing } from 'lit-html';
+import { AsyncDirective, directive } from 'lit-html/async-directive.js';
+import { ChildPart } from 'lit-html/directive.js';
+
+class ProvideDirective<T> extends AsyncDirective {
+	value!: T;
+	context!: Context<T>;
+
+	listeners: Set<(value: T) => void> = new Set();
+	cleanup!: VoidFunction | undefined;
+
+	update(part: ChildPart, [context, value]: [Context<T>, T]) {
+		this.context = context;
+
+		if (this.value !== value) {
+			this.value = value;
+
+			for (const callback of this.listeners) {
+				callback(value);
+			}
+		}
+
+		if (!this.cleanup) {
+			part.parentNode.addEventListener(contextEvent, this);
+			this.cleanup = () =>
+				part.parentNode.removeEventListener(contextEvent, this);
+		}
+
+		return noChange;
+	}
+
+	render() {
+		return nothing;
+	}
+
+	handleEvent(event: CustomEvent<ContextDetail<T>>): void {
+		const { detail } = event;
+
+		if (detail.Context !== this.context) return;
+
+		detail.value = this.value;
+		detail.unsubscribe = this.unsubscribe.bind(this, detail.callback);
+
+		this.listeners.add(detail.callback);
+
+		event.stopPropagation();
+	}
+
+	unsubscribe(callback: (value: T) => void): void {
+		this.listeners.delete(callback);
+	}
+
+	protected disconnected(): void {
+		this.cleanup?.();
+		this.cleanup = undefined;
+	}
+}
+
+export const provide = directive(ProvideDirective);
