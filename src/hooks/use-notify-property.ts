dfe1ed43f0f1e3b57d2ffe02f44a855315d7e1a9
Meghea Iulian <iulian.meghea@gmail.com>
2023-09-05 23:05:47 +0300

    feat(use-notify-property): adjust type

--- src/hooks/use-notify-property.ts
+++ src/hooks/use-notify-property.ts
@@ -1,40 +1,39 @@
 import { useEffect } from 'haunted';
 import { useHost } from './use-host';
 
-const UPPER = /([A-Z])/gu,
-	/* Emulate polymer notify props */
-	notifyProperty = <
-		K extends string,
-		T extends HTMLElement & { [key in K]?: unknown }
-	>(
-		host: T,
-		name: K,
-		value: T[K]
-	) => {
-		// this is required to make polymer double-binding recognize the change
-		// @see https://github.com/Polymer/polymer/blob/76c71e186ecc605294c3575dd31ac7983a8b3ae3/lib/mixins/property-effects.js#L382
-		host[name] = value;
+const UPPER = /([A-Z])/gu;
 
-		// emulate polymer notify event
-		host.dispatchEvent(
-			new CustomEvent(name.replace(UPPER, '-$1').toLowerCase() + '-changed', {
-				detail: { value },
-			})
-		);
-	},
-	useNotifyProperty = <
-		T extends HTMLElement,
-		K extends Extract<keyof T, string>,
-		V extends T[K]
-	>(
-		name: K,
-		value: V,
-		deps: unknown[] = [value]
-	) => {
-		const host = useHost<T>();
-		useEffect(() => {
-			notifyProperty(host, name, value);
-		}, deps);
-	};
+/* Emulate polymer notify props */
+export const notifyProperty = <
+	K extends string,
+	T extends HTMLElement & { [key in K]?: unknown },
+>(
+	host: T,
+	name: K,
+	value: T[K],
+) => {
+	// this is required to make polymer double-binding recognize the change
+	// @see https://github.com/Polymer/polymer/blob/76c71e186ecc605294c3575dd31ac7983a8b3ae3/lib/mixins/property-effects.js#L382
+	host[name] = value;
 
-export { notifyProperty, useNotifyProperty };
+	// emulate polymer notify event
+	host.dispatchEvent(
+		new CustomEvent(name.replace(UPPER, '-$1').toLowerCase() + '-changed', {
+			detail: { value },
+		}),
+	);
+};
+
+export const useNotifyProperty = <
+	K extends string,
+	V,
+>(
+	name: K,
+	value: V,
+	deps: unknown[] = [value],
+) => {
+	const host = useHost<HTMLElement & {[key in K]: V}>();
+	useEffect(() => {
+		notifyProperty(host, name, value as any);
+	}, deps);
+};
