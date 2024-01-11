5f85d6170d4655fa07872759073a7512622121be
Meghea Iulian <iulian.meghea@gmail.com>
2024-01-11 14:43:55 +0200

    feat: update exported type def

--- src/elements/cz-icon.ts
+++ src/elements/cz-icon.ts
@@ -1,5 +1,5 @@
 import { html, svg } from 'lit-html';
-import { ifDefined } from 'lit-html/directives/if-defined';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { component } from '@pionjs/pion';
 
 interface Ico {
