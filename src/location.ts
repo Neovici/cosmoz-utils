02c0001a2b2930a9b4164625544a51a81bf84740
Cristian Necula <virusz@gmail.com>
2025-08-19 09:17:40 +0300

    fix(singleParse): should return undefined when the param is not part of the url
    Fixes #204

--- src/location.ts
+++ src/location.ts
@@ -3,22 +3,25 @@ import { identity } from './function';
 export const hashUrl = () =>
 		new URL(
 			location.hash.replace(/^#!?/iu, '').replace('%23', '#'),
-			location.origin
+			location.origin,
 		),
 	singleParse = (hashParam: string, codec = identity) => {
 		const values = new URLSearchParams(hashUrl().hash.replace('#', '')).getAll(
-			hashParam
+			hashParam,
 		);
 
-		if (Array.isArray(values)) {
-			return values.length === 1 ? codec(values[0]) : values.map(codec);
+		switch (values.length) {
+			case 0:
+				return undefined;
+			case 1:
+				return codec(values[0]);
+			default:
+				return values.map(codec);
 		}
-
-		return codec(values);
 	},
 	multiParse = (hashParam: string, codec = identity) => {
 		const params = Array.from(
-			new URLSearchParams(hashUrl().hash.replace('#', '')).entries()
+			new URLSearchParams(hashUrl().hash.replace('#', '')).entries(),
 		)
 			.filter(([param]) => param.startsWith(hashParam))
 			.map(([param, value]) => codec([param.replace(hashParam, ''), value]))
