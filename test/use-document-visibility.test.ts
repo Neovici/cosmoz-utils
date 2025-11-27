134ad1c53c19462e963d939df9a8db31269c4d3c
Husoschi Ionut-Catalin <ionuthusoschi@gmail.com>
2025-11-27 16:15:38 +0200

    feat: useDocumentVisibility hook (#213)
    * feat: useDocumentVisibility hook
    
    * fix: value removed and replace expect with assert in function call
    
    * fix: change useDocumentVisibility to return boolean instead of string
    
    * fix: remove expect from test file

---
+++ test/use-document-visibility.test.ts
@@ -0,0 +1,66 @@
+import { assert, fixture, html } from '@open-wc/testing';
+import { component } from '@pionjs/pion';
+import { spy } from 'sinon';
+import { useDocumentVisibility } from '../src/hooks/use-document-visibility';
+
+interface UseDocumentVisibilityElement extends HTMLElement {
+	visibility: boolean;
+}
+// set a custom property on custom-webcomponent
+customElements.define(
+	'use-document-visibility',
+	component((host) => {
+		host.visibility = useDocumentVisibility();
+	}),
+);
+
+suite('useDocumentVisibility', () => {
+	test('should return initial document visibility state', async () => {
+		const result = (await fixture(
+			html`<use-document-visibility></use-document-visibility>`,
+		)) as UseDocumentVisibilityElement;
+
+		assert.isTrue(result.visibility);
+	});
+
+	test('should update when visibility changes', async () => {
+		let mockVisibility = 'visible';
+		Object.defineProperty(document, 'visibilityState', {
+			get() {
+				return mockVisibility;
+			},
+			configurable: true,
+		});
+
+		const result = (await fixture(
+			html`<use-document-visibility></use-document-visibility>`,
+		)) as UseDocumentVisibilityElement;
+		assert.isTrue(result.visibility);
+
+		mockVisibility = 'hidden';
+
+		const event = new Event('visibilitychange');
+		document.dispatchEvent(event);
+
+		await new Promise((resolve) => setTimeout(resolve, 10));
+		assert.isFalse(result.visibility);
+	});
+
+	test('should attach and remove event listener', async () => {
+		const addEventListenerSpy = spy(document, 'addEventListener');
+		const removeEventListenerSpy = spy(document, 'removeEventListener');
+
+		const result = await fixture(
+			html`<use-document-visibility></use-document-visibility>`,
+		);
+
+		assert.isTrue(addEventListenerSpy.calledWith('visibilitychange'));
+
+		result.remove();
+
+		assert.isTrue(removeEventListenerSpy.calledWith('visibilitychange'));
+
+		addEventListenerSpy.restore();
+		removeEventListenerSpy.restore();
+	});
+});
