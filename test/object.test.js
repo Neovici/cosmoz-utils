376a7c22c9a96f0bd6efa3064046b931c581a350
Cristian Necula <virusz@gmail.com>
2026-05-27 10:16:20 +0000

    fix: prop handles primitive values gracefully
    Change prop(key) to return the value as-is when it's not a non-null
    object, instead of returning undefined. This fixes cell rendering,
    XLSX export, tooltips, grouping, and filtering in cosmoz-omnitable
    when value-path points to a primitive.
    
    FE-726

--- test/object.test.js
+++ test/object.test.js
@@ -1,6 +1,6 @@
 import { assert } from '@open-wc/testing';
 import { identity } from '../src/function';
-import { prop, props, strProp, merge, omit } from '../src/object';
+import { merge, omit, prop, props, strProp } from '../src/object';
 
 suite('prop', () => {
 	test('prop', () => {
@@ -9,9 +9,29 @@ suite('prop', () => {
 		assert.equal(prop(''), identity);
 	});
 
+	test('prop passes through primitives', () => {
+		assert.equal(prop('key')('hello'), 'hello');
+		assert.equal(prop('key')(42), 42);
+		assert.isTrue(prop('key')(true));
+		assert.isNull(prop('key')(null));
+		assert.isUndefined(prop('key')(undefined));
+	});
+
+	test('prop with objects unchanged', () => {
+		assert.equal(prop('key')({ key: 'val' }), 'val');
+		assert.isUndefined(prop('key')({ other: 1 }));
+	});
+
 	test('strProp', () => {
 		assert.equal(strProp('b')({ b: 2 }), '2');
 		assert.equal(strProp('b')({ c: 4 }), '');
+	});
+
+	test('strProp passes through primitives', () => {
+		assert.equal(strProp('key')(42), '42');
+		assert.equal(strProp('key')('hello'), 'hello');
+		assert.equal(strProp('key')(null), '');
+		assert.equal(strProp('key')(undefined), '');
 	});
 });
 
@@ -27,12 +47,12 @@ suite('merge', () => {
 					a: 1,
 					b: 3,
 				},
-				{ b: 2 }
+				{ b: 2 },
 			),
 			{
 				a: 1,
 				b: 2,
-			}
+			},
 		);
 		assert.deepEqual(
 			merge(
@@ -41,12 +61,12 @@ suite('merge', () => {
 					b: 3,
 				},
 				{ b: 2 },
-				null
+				null,
 			),
 			{
 				a: 1,
 				b: 2,
-			}
+			},
 		);
 	});
 
